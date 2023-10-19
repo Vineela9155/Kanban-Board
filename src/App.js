b41fcea6068774af18690c6244ffc4476f7fc670
Vineela9155 <108358128+Vineela9155@users.noreply.github.com>
2023-10-19 17:10:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
 import './App.css'
 import {TbPointFilled} from "react-icons/tb"
-import {GrAdd} from "react-icons/gr"
-import {SlOptions} from "react-icons/sl"
-import { FiArrowDown} from "react-icons/fi"
 
 const App = () => {
   const [tickets, setTickets] = useState([]);
-  const [groupingOption, setGroupingOption] = useState('status');
-  const [sortingOption, setSortingOption] = useState(null);
+  const [gpo, setgpo] = useState('priority');
+  const [sto, setsto] = useState(null);
 
   useEffect(() => {
-    const fetchUpc  = async()=>{
-      const {data : {tickets}} = await axios.get(`https://api.quicksell.co/v1/internal/frontend-assignment`);
-      setTickets(tickets);
-    };
-    fetchUpc();
+    fetch('https://api.quicksell.co/v1/internal/frontend-assignment')
+      .then((response) => response.json())
+      .then((data) => setTickets(data));
   }, []);
 
 
-  const sortOptions = ['Priority', 'Title'];
-  const groupOptions = ['Status', 'User', 'Priority'];
+  const so = ['Priority', 'Title'];
+  const go = ['Priority', 'User', 'Status'];
 
-  const groupedAndSortedTickets = () => {
-    let grouped = {};
-    for (const ticket of tickets) {
-      const groupKey = groupingOption === 'Status' ? ticket.status : groupingOption === 'User' ? ticket.userId : ticket.priority;
-      if (!grouped[groupKey]) grouped[groupKey] = [];
-      grouped[groupKey].push(ticket);
+  const fun = () => {
+    let arr = {};
+    for (const x of tickets) {
+      const groupKey = gpo === 'Status' ? x.status : gpo === 'User' ? x.userId : tickets.priority;
+      arr[groupKey].push(x);
     }
 
-    for (const group in grouped) {
-      if (sortingOption === 'Priority') {
-        grouped[group].sort((a, b) => b.priority > a.priority);
+    for (const x in arr) {
+      if (sto === 'Priority') {
+        arr[x].sort((a, b) => b.priority > a.priority);
       } else if (sortingOption === 'Title') {
-        grouped[group].sort((a, b) => a.title.localeCompare(b.title));
+        arr[x].sort((a, b) => a.title >b.title);
       }
     }
-    return grouped;
+    return arr;
   };
 
   return (
     <div>
       <div className='nav'>
-        <div class="dropdown">
-          <button class="dropbtn">  Display
-          </button>
-        <div class="dropdown-content">
      
       <div >
         <label>Group by: </label>
         <select onChange={(e) => setGroupingOption(e.target.value)} value={groupingOption}>
-          {groupOptions.map((option, index) => (
-            <option key={index} value={option} className='sltr'>{option}</option>
+          {go.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
           ))}
         </select>
       </div>
       <div>
         <label>Sort by: </label>
         <select onChange={(e) => setSortingOption(e.target.value)} value={sortingOption}>
-          {sortOptions.map((option, index) => (
-            <option key={index} value={option} className='sltr'>{option}</option>
+          {so.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
           ))}
         </select>
       </div>
-      </div>
-  </div>
       </div>
       <div className='main'>
       <h2 className='name'>Grouping by <b>{groupingOption}</b> is selected.</h2>
@@ -74,22 +61,14 @@ const App = () => {
         {Object.entries(groupedAndSortedTickets()).map(([group, groupTickets]) => (
           <div key={group} className='col'>
             <div className='name' >
-                <span className='nameset'>{group}</span>
-                <p className='addsym'><button><GrAdd/></button>   
-                    <span class="dropdown3">
-                      <button class="dropbtn3"> <SlOptions/> 
-                      </button>
-                      <span class="dropdown-content3">
-                        <p>Options</p>
-                      </span>
-                    </span>
-                 </p>       
+                <span className='nameset'>{group}</span>     
             </div>
             
             {groupTickets.map((ticket) => (
               <div key={ticket.id} className='box'>
                 <p className='userman'>{ticket.userId}</p>
-                <div className='na'><TbPointFilled/>{ticket.title}</div>          
+                <div className='na'><TbPointFilled/>{ticket.title}</div>  
+                <p>{ticket.priority}</p>        
               </div>
             ))}
           </div>
@@ -100,4 +79,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App;
